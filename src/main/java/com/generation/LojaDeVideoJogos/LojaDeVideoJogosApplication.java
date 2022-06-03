2ff93166e1a204e26f3cbd04b9fc68c5ba49aa7f
Your Name <armandoH99>
2022-06-03 20:06:32 -0300

    back end de loja de videojogos

---
+++ src/main/java/com/generation/LojaDeVideoJogos/LojaDeVideoJogosApplication.java
@@ -0,0 +1,13 @@
+package com.generation.LojaDeVideoJogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeVideoJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeVideoJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/LojaDeVideoJogos/LojaDeVideoJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.LojaDeVideoJogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeVideoJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
